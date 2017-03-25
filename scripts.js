3799eb0a8a394d17835740862c8041bb61a65890
zosia <zofiaszczech1@gmail.com>
2017-03-25 15:38:36 +0100

    add

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function rysujChoinke(x) {
+
+	for (var i = 0; i < x ; i++); {
+		var star = '*';  
+
+		for (var n = 0; n < i*2-1; n++) {
+			var star = star +='*';
+		}
+
+		console.log(star);
+	}
+}
+
+rysujChoinke(5);
+
+
+
